c4c8827a3e5f9356a09a2a097160402bececdd8f
Yang-Li-Chen <2572958006@qq.com>
2020-06-17 13:27:46 +0800

    服务器

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 var express = require("express")
 let app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+app.use(function(req,res,next){
+    res.header('Access-Control-Allow-Origin','*');//跨域访问
+    next();
+});
+var session = require("express-session");
+app.use(session({
+    secret:'secret',
+    resave:true,
+    saveUninitialized:false,
+
+}));
+app.use(function(req, res, next){
+    res.locals.user = req.session.user;
+    var err = req.session.error;
+    res.locals.message = '';
+    if (err) res.locals.message = '<div style="margin-bottom: 20px;color:red;">' + err + '</div>';
+    next();
+});
+
+
+
 
 app.listen(3000,()=>{
     console.log("服务器启动了~")
